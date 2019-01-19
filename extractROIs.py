64132ea37b1a745e18d3787fad9c01ac777c5191
jquach12 <jonq1226@gmail.com>
2019-01-19 15:29:16 -0800

    added ground truths and manages to get corresponding ROIs

--- extractROIs.py
+++ extractROIs.py
@@ -5,10 +5,19 @@ import json
 
 CSV_PATH = '/Users/silver/Desktop/buildUCLA/stripped-book-annotation-classification-classifications-11June2018.csv'
 
+# ASSUMES FILES ARE .PNG FORMAT
+META_DATA_PATH = '/Users/silver/Desktop/buildUCLA/phase2/annotations-computervision/books meta data.csv'
+
 # WARNING CSV file is inconsistent with the key identifier for the key names. For now, we will only retain the data that uses 'filename' as the key to retrieve the file name
 def extractROIs(csv_file_path):
 	assert csv_file_path
 	df = pd.read_csv(csv_file_path, usecols=['annotations', 'subject_data','subject_ids'])
+
+	# need to add this bit to map file names to the randomized IDs
+	df_meta = pd.read_csv(META_DATA_PATH, usecols=['ID','File Name'])
+	name2id = dict()
+	for index, row in df_meta.iterrows():
+		name2id[row[1].replace('.png','.jpg')] = row[0]
 
 	fileNames = []
 	coordinates = []
@@ -21,11 +30,14 @@ def extractROIs(csv_file_path):
 		s_data = json.loads(row['subject_data'])
 
 		try: # if this succeeds, then we can retrieve its annotations as well
-			fileNames.append(s_data[s_id]['filename'])
+			
+			fn = s_data[s_id]['filename']
+
+			print(name2id[fn])
+
+			fileNames.append(name2id[s_data[s_id]['filename']])
 			
 			tasks = json.loads(df.iloc[index]['annotations'])
-
-			#print(tasks)
 
 			imageCoordinates = []
 
