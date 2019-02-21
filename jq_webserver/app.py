e3261340225832a9a424959df6daf038ce930fae
jquach12 <jonq1226@gmail.com>
2019-02-21 12:12:53 -0800

    made a more informative comment

--- jq_webserver/app.py
+++ jq_webserver/app.py
@@ -15,7 +15,7 @@ def submit():
 
    results = infer(formattedInput)  
    
-   #USE THIS results if you're just trying to test displaying images
+   #USE THIS results TO TEST DISPLAYING IMAGES
    #results = ['https://previews.123rf.com/images/lufimorgan/lufimorgan1509/lufimorgan150900005/45154060-blue-eyes-siberian-husky-puppy-lyingand-looking-on-green-grass.jpg', 'https://ih1.redbubble.net/image.451672470.1427/flat,550x550,075,f.u4.jpg']
 
    return render_template('results.html', imgURIs = results)
