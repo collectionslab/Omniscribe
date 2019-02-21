adc7c9215d42ae111fc17a128e9a1fda6c816244
jquach12 <jonq1226@gmail.com>
2019-02-21 12:11:15 -0800

    added test case for app.py

--- jq_webserver/app.py
+++ jq_webserver/app.py
@@ -10,9 +10,16 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
    rawInput = request.form['textbox']
+
    formattedInput = rawInput.replace('\r\n',' ').split()
+
    results = infer(formattedInput)  
+   
+   #USE THIS results if you're just trying to test displaying images
+   #results = ['https://previews.123rf.com/images/lufimorgan/lufimorgan1509/lufimorgan150900005/45154060-blue-eyes-siberian-husky-puppy-lyingand-looking-on-green-grass.jpg', 'https://ih1.redbubble.net/image.451672470.1427/flat,550x550,075,f.u4.jpg']
+
    return render_template('results.html', imgURIs = results)
+
 
 if __name__ == '__main__':
    app.run()
