baad98343443a282b380ad10274c0d828c70d136
jquach12 <jonq1226@gmail.com>
2019-02-21 12:09:15 -0800

    cleaned upp app.py

--- jq_webserver/app.py
+++ jq_webserver/app.py
@@ -10,16 +10,9 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
    rawInput = request.form['textbox']
-
    formattedInput = rawInput.replace('\r\n',' ').split()
-
    results = infer(formattedInput)  
-   #results = ['https://previews.123rf.com/images/lufimorgan/lufimorgan1509/lufimorgan150900005/45154060-blue-eyes-siberian-husky-puppy-lyingand-looking-on-green-grass.jpg', 'https://ih1.redbubble.net/image.451672470.1427/flat,550x550,075,f.u4.jpg']
-   print(results)
-
    return render_template('results.html', imgURIs = results)
-
-
 
 if __name__ == '__main__':
    app.run()
